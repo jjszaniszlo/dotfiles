811b9f44e88450fe6333f58dad13d7e0143d6753
John Szaniszlo <Johnszaniszlo@gmail.com>
2023-05-04 23:50:48 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,1 @@
+local M = require 'lua.init'
---
+++ lua/plugins/core.lua
@@ -0,0 +1,113 @@
+return {
+  { -- kanagawa
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup {
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none"
+              },
+            },
+          },
+        },
+      }
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+
+  { -- tree-sitter
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = { "c", "cpp", "lua", "tsx", "typescript", "vimdoc", "vim", "zig", "javascript", "html", "css" },
+
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = false,
+
+        highlight = { enable = true },
+        indent = { enable = true, disable = { "python" } },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<c-space>",
+            node_incremental = "<c-space>",
+            scope_incremental = "<c-s>",
+            node_decremental = "<M-space>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
+        },
+      }
+    end
+  },
+
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup{}
+    end,
+  },
+
+  { -- lualine
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = "kanagawa",
+        component_separators = "|",
+        section_separators = "",
+      },
+    },
+  },
+}
