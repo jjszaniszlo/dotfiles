a8bb989a8d318cdce1f797bda17d32dcf5e4df40
jjszaniszlo <johnszaniszlo@gmail.com>
2024-02-27 17:34:49 -0800

    move nvim to proper place

--- nvim/lua/custom/configs/neovide.lua
+++
@@ -1,9 +0,0 @@
-
-vim.o.guifont = "Victor Mono:h20"
-vim.g.neovide_window_blurred = true
-vim.g.neovide_transparency = 0.9
-
-vim.g.neovide_scroll_animation_length = 0.3
-vim.g.neovide_cursor_animation_length = 0.01
-vim.g.neovide_cursor_trail_size = 0.3
-vim.g.neovide_cursor_antialiasing = true
