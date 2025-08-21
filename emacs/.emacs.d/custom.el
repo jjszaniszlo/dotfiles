c2cd91a33584dafdd75ee573aad2fde12e04698c
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-08-21 15:56:38 -0700

    add custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,11 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(autothemer darkroom diff-hl dir-config exec-path-from-shell golden-ratio
-                json-mode magit markdown-mode mixed-pitch move-text nix-mode
-                no-littering origami toml-mode treesit-auto ultra-scroll
-                yaml-mode))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((ultra-scroll :url "https://github.com/jdtsmith/ultra-scroll" :branch "main")))
  '(safe-local-variable-values
